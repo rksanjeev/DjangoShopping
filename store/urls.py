1ab71ae02866cb4b2c480cfb0bc79ce0bda411e2
Sanjeev Kumar <r.sanjeev@outlook.com>
2021-04-14 23:19:53 +0530

    added rupee symbol

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.list_products, name = 'listproducts'),
+    path('', views.list_products, name = 'all_products'),
     path('item/<slug:slug>/', views.product_detail, name= 'product_detail'),
     path('search/<slug:slug>/', views.category_list, name= 'category_list'),
 

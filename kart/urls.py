aae23d7935fda0d2c714dbb663110f28396c93b0
Sanjeev Kumar <r.sanjeev@outlook.com>
2021-07-21 21:36:16 +0530

    update and delete items in cart

--- kart/urls.py
+++ kart/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'kart'
 
 urlpatterns = [
-    path('',views.cart_summary, name='summary'),
+    path('summary/',views.cart_summary, name='summary'),
     path('add_item/', views.cart_add_item, name ='cart_add_item'),
-
-
+    path('remove_item/', views.cart_remove_item, name ='basket_delete'),
+    path('update_item/', views.cart_update_item, name ='basket_update'),
 ]
--- kart/views.py
+++ kart/views.py
@@ -6,7 +6,8 @@ from store.models import Product
 # Create your views here.
 
 def cart_summary(request):
-    return render(request, 'cart/summary.html')
+    cart = Kart(request=request)
+    return render(request, 'cart/summary.html', {'cart': cart})
 
 def cart_add_item(request):
     cart = Kart(request)
@@ -15,6 +16,29 @@ def cart_add_item(request):
         product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
         cart.add(product=product, quantity=product_qty)
-    cart_qty = cart.size()
-    response = JsonResponse({'qty':cart_qty})
+    basketqty = cart.size()
+    baskettotal = cart.get_total_price()
+    response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+    return response
+
+def cart_remove_item(request):
+    cart=Kart(request)
+    if request.POST.get('action') == 'delete':
+        product_id = int(request.POST.get('productid'))
+        cart.remove(product=product_id)
+
+        basketqty = cart.size()
+        baskettotal = cart.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+def cart_update_item(request):
+    cart = Kart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        cart.update(product=product_id, quantity=product_qty)
+    basketqty = cart.size()
+    baskettotal = cart.get_total_price()
+    response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
     return response
